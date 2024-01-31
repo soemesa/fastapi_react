0217f3f47263a8d4d5bbfc243f25a6735f249e65
Soilenyis <soilenyis@gmail.com>
2024-01-31 08:26:26 -0300

    criando modelos e migrations

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import String, ForeignKey
+
+from sqlalchemy import String, ForeignKey, Float
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -13,8 +14,8 @@ class Produto(BaseModel):
     nome: Mapped[str] = mapped_column(String(30), nullable=False)
     quantidade_estoque: Mapped[int] = mapped_column(default=0)
     quantidade_vendida: Mapped[int] = mapped_column(default=0)
-    preco_unico: Mapped[float] = mapped_column(max_digits=8, decimal_places=2, default=0.00)
-    receita: Mapped[float] = mapped_column(max_digits=20, decimal_places=2, default=0.00)
+    preco_unico: Mapped[float] = mapped_column(Float(precision=8, decimal_return_scale=2), default=0.00)
+    receita: Mapped[float] = mapped_column(Float(precision=8, decimal_return_scale=2), default=0.00)
 
     fornecedor: Mapped[int] = mapped_column(ForeignKey('fornecedor.id'))
 
